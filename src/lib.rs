09d8e3fbff70ea4f59c33fa0db06884d667be15e
Antti Valli <antti.valli@gmail.com>
2018-03-30 09:22:36 +0300

    Refactor things into lib

---
+++ src/lib.rs
@@ -0,0 +1,124 @@
+
+use std::fmt;
+
+pub struct Coord(u32);
+
+impl Coord
+{
+    pub fn new( row: u32, col: u32 ) -> Coord
+    {
+        if col > 7 || row > 7
+        {
+            panic!("Overindexing the board row {}, col {}", row, col);
+        }
+        Coord( col + row * 8 )
+    }
+    pub fn get_row( &self ) -> u32
+    {
+        self.0 / 8
+    }
+    pub fn get_col( &self ) -> u32
+    {
+        self.0 % 8
+    }
+    pub fn get_idx( &self ) -> u32
+    {
+        self.0
+    }
+}
+
+impl fmt::Display for Coord {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "({}, {})", self.get_row(), self.get_col())
+    }
+}
+
+pub struct BitBoard(u64);
+
+impl BitBoard
+{
+    pub fn empty() -> BitBoard
+    {
+        BitBoard(0)
+    }
+    pub fn get_value_at( &self, coord: Coord ) -> bool
+    {
+        ( self.0 >> coord.get_idx() ) & 1 == 1
+    }
+
+    pub fn set_value_at( &mut self, coord: Coord, value: bool )
+    {
+        let idx = coord.get_idx();
+        if value
+        {
+            self.0 |= 1 << idx;
+        }
+        else
+        {
+            self.0 &= !(1 << idx);       
+        }
+    }
+
+    pub fn shift_up( &self ) -> BitBoard
+    {
+        BitBoard( self.0 >> 8 )
+    }
+
+    pub fn shift_down( &self ) -> BitBoard
+    {
+        BitBoard( self.0 << 8 )
+    }
+
+    pub fn shift_left( &self ) -> BitBoard
+    {
+        let mask = BitBoard::leftmost_column().0;
+        BitBoard( ( self.0 & !mask ) >> 1 )
+    }
+
+    pub fn shift_right( &self ) -> BitBoard
+    {
+        let mask = BitBoard::rightmost_column().0;
+        BitBoard( ( self.0 & !mask ) << 1 )
+    }
+
+
+    pub fn rightmost_column() -> BitBoard
+    {
+        BitBoard( 0b1000000010000000100000001000000010000000100000001000000010000000u64 )
+    }
+
+    pub fn leftmost_column() -> BitBoard
+    {
+        BitBoard( 0b0000000100000001000000010000000100000001000000010000000100000001u64 )
+    }
+    
+    pub fn first_one(&self) -> Coord
+    {
+        Coord( self.0.trailing_zeros() )
+    }
+
+}
+
+impl fmt::Display for BitBoard
+{
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for row in 0..8
+        {
+            write!(f, "\n")?;
+            for col in 0..8
+            {
+
+                if self.get_value_at(Coord::new(row, col))
+                {
+                    write!(f, "1")?;
+                }
+                else 
+                {
+                    write!(f, "0")?;
+                }
+            }
+        }
+        write!(f,"")
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,124 +1,10 @@
-use std::fmt;
-
-struct Coord(u32);
-
-impl Coord
-{
-    fn new( row: u32, col: u32 ) -> Coord
-    {
-        if col > 7 || row > 7
-        {
-            panic!("Overindexing the board row {}, col {}", row, col);
-        }
-        Coord( col + row * 8 )
-    }
-    fn get_row( &self ) -> u32
-    {
-        self.0 / 8
-    }
-    fn get_col( &self ) -> u32
-    {
-        self.0 % 8
-    }
-    fn get_idx( &self ) -> u32
-    {
-        self.0
-    }
-}
-
-impl fmt::Display for Coord {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "({}, {})", self.get_row(), self.get_col())
-    }
-}
-
-struct BitBoard(u64);
-
-impl BitBoard
-{
-    fn get_value_at( &self, coord: Coord ) -> bool
-    {
-        ( self.0 >> coord.get_idx() ) & 1 == 1
-    }
-
-    fn set_value_at( &mut self, coord: Coord, value: bool )
-    {
-        let idx = coord.get_idx();
-        if value
-        {
-            self.0 |= 1 << idx;
-        }
-        else
-        {
-            self.0 &= !(1 << idx);       
-        }
-    }
-
-    fn shift_up( &self ) -> BitBoard
-    {
-        BitBoard( self.0 >> 8 )
-    }
-
-    fn shift_down( &self ) -> BitBoard
-    {
-        BitBoard( self.0 << 8 )
-    }
-
-    fn shift_left( &self ) -> BitBoard
-    {
-        let mask = BitBoard::leftmost_column().0;
-        BitBoard( ( self.0 & !mask ) >> 1 )
-    }
-
-    fn shift_right( &self ) -> BitBoard
-    {
-        let mask = BitBoard::rightmost_column().0;
-        BitBoard( ( self.0 & !mask ) << 1 )
-    }
-
-
-    fn rightmost_column() -> BitBoard
-    {
-        BitBoard( 0b1000000010000000100000001000000010000000100000001000000010000000u64 )
-    }
-
-    fn leftmost_column() -> BitBoard
-    {
-        BitBoard( 0b0000000100000001000000010000000100000001000000010000000100000001u64 )
-    }
-    
-    fn first_one(&self) -> Coord
-    {
-        Coord( self.0.trailing_zeros() )
-    }
-}
-
-impl fmt::Display for BitBoard
-{
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        for row in 0..8
-        {
-            write!(f, "\n")?;
-            for col in 0..8
-            {
-
-                if self.get_value_at(Coord::new(row, col))
-                {
-                    write!(f, "1")?;
-                }
-                else 
-                {
-                    write!(f, "0")?;
-                }
-            }
-        }
-        write!(f,"")
-    }
-}
+extern crate bit_board;
+use bit_board::Coord;
+use bit_board::BitBoard;
 
 fn test_pattern( ) -> BitBoard
 {
-    let mut board = BitBoard(0);
+    let mut board = BitBoard::empty();
     board.set_value_at( Coord::new(0, 0), true );
     board.set_value_at( Coord::new(0, 7), true );
     board.set_value_at( Coord::new(7, 0), true );
@@ -135,6 +21,4 @@ fn main() {
     println!("first one at: {}",test_board.shift_up().first_one());
     println!("Shift left: {}", test_board.shift_left());
     println!("Shift down: {}", test_board.shift_down());
-
-
 }
