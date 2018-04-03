2f4882452ae0958321360f483734cb9170548ba7
Antti Valli <Antti.Valli@gmail.com>
2018-04-03 21:16:20 +0300

    divide things into modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,301 +1,96 @@
+pub mod bitboard;
 
-use std::fmt;
-use std::ops::{BitOr, BitAnd, BitOrAssign, BitAndAssign, Not};
-
-#[derive(Clone,Copy)]
-pub struct Coord(u32);
-
-impl Coord
+pub mod Othello
 {
-    pub fn new( row: u32, col: u32 ) -> Coord
+    use std::fmt;
+    use super::bitboard::{Coord, Direction, BitBoard};
+    enum Player
     {
-        if col > 7 || row > 7
+        Black,
+        White
+    }
+
+    pub struct OthelloSituation
+    {
+        black_board: BitBoard,
+        white_board: BitBoard,
+        moves: BitBoard,
+        turn: Player
+    }
+
+    fn generate_moves_towards( own_board: BitBoard, other_board: BitBoard, direction: Direction ) -> BitBoard
+    {
+        let empty_spaces = !own_board & !other_board;
+        let mut moves = BitBoard::empty();
+        let mut move_tmp = own_board.shift( direction ) & other_board;
+        while !(move_tmp.is_empty())
         {
-            panic!("Overindexing the board row {}, col {}", row, col);
+            move_tmp = move_tmp.shift( direction );
+            let moves_generated_this_round = move_tmp & empty_spaces;
+            move_tmp &= other_board;
+            moves |= moves_generated_this_round;
         }
-        Coord( col + row * 8 )
-    }
-    pub fn get_row( &self ) -> u32
-    {
-        self.0 / 8
-    }
-    pub fn get_col( &self ) -> u32
-    {
-        self.0 % 8
-    }
-    pub fn get_idx( &self ) -> u32
-    {
-        self.0
-    }
-}
-
-impl fmt::Display for Coord {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "({}, {})", self.get_row(), self.get_col())
-    }
-}
-
-#[derive(Clone,Copy)]
-pub enum Direction
-{
-    Right,
-    UpRight,
-    Up,
-    UpLeft,
-    Left,
-    DownLeft,
-    Down,
-    DownRight
-}
-
-#[derive(Debug, Copy, Clone)]
-pub struct BitBoard(u64);
-
-impl BitBoard
-{
-    pub fn empty() -> BitBoard
-    {
-        BitBoard(0)
+        moves
     }
 
-    pub fn rightmost_column() -> BitBoard
+    fn generate_moves( own_board: BitBoard, other_board: BitBoard ) -> BitBoard
     {
-        BitBoard( 0b1000000010000000100000001000000010000000100000001000000010000000u64 )
+        generate_moves_towards( own_board, other_board, Direction::Right ) |
+        generate_moves_towards( own_board, other_board, Direction::UpRight ) |
+        generate_moves_towards( own_board, other_board, Direction::Up ) |
+        generate_moves_towards( own_board, other_board, Direction::UpLeft ) |
+        generate_moves_towards( own_board, other_board, Direction::Left ) |
+        generate_moves_towards( own_board, other_board, Direction::DownLeft ) |
+        generate_moves_towards( own_board, other_board, Direction::Down ) |
+        generate_moves_towards( own_board, other_board, Direction::DownRight )
     }
 
-    pub fn leftmost_column() -> BitBoard
+    impl OthelloSituation
     {
-        BitBoard( 0b0000000100000001000000010000000100000001000000010000000100000001u64 )
-    }
-    
-    pub fn is_empty(&self) -> bool
-    {
-        self.0 == 0
-    }
-    pub fn get_value_at( &self, coord: Coord ) -> bool
-    {
-        ( self.0 >> coord.get_idx() ) & 1 == 1
-    }
-
-    pub fn set_value_at( &mut self, coord: Coord, value: bool )
-    {
-        let idx = coord.get_idx();
-        if value
+        pub fn new() -> OthelloSituation
         {
-            self.0 |= 1 << idx;
-        }
-        else
-        {
-            self.0 &= !(1 << idx);       
+            let mut black_board = BitBoard::empty();
+            black_board.set_value_at(Coord::new(3,4),true);
+            black_board.set_value_at(Coord::new(4,3),true);
+            let mut white_board = BitBoard::empty();
+            white_board.set_value_at(Coord::new(3,3),true);
+            white_board.set_value_at(Coord::new(4,4),true);
+            let moves = generate_moves( black_board, white_board );
+            OthelloSituation{ black_board, white_board, moves, turn: Player::Black }
         }
     }
 
-    pub fn shift_up( &self ) -> BitBoard
+    impl fmt::Display for OthelloSituation
     {
-        BitBoard( self.0 >> 8 )
-    }
-
-    pub fn shift_down( &self ) -> BitBoard
-    {
-        BitBoard( self.0 << 8 )
-    }
-
-    pub fn shift_left( &self ) -> BitBoard
-    {
-        let mask = BitBoard::leftmost_column().0;
-        BitBoard( ( self.0 & !mask ) >> 1 )
-    }
-
-    pub fn shift_right( &self ) -> BitBoard
-    {
-        let mask = BitBoard::rightmost_column().0;
-        BitBoard( ( self.0 & !mask ) << 1 )
-    }
-
-    pub fn shift( &self, direction: Direction ) -> BitBoard
-    {
-        match direction
-        {
-            Direction::Right => self.shift_right(),
-            Direction::UpRight => self.shift_right().shift_up(),
-            Direction::Up => self.shift_up(),
-            Direction::UpLeft => self.shift_left().shift_up(),
-            Direction::Left => self.shift_left(),
-            Direction::DownLeft => self.shift_left().shift_down(),
-            Direction::Down => self.shift_down(),
-            Direction::DownRight => self.shift_right().shift_down()
-        }
-    }
-
-    pub fn first_one(&self) -> Option<Coord>
-    {
-        if self.is_empty()
-        {
-            None
-        }
-        else 
-        {
-            Some(Coord( self.0.trailing_zeros() ))
-        }
-    }
-
-}
-
-impl PartialEq for BitBoard
-{
-    fn eq(&self, other: &BitBoard) -> bool
-    {
-        self.0 == other.0
-    }
-}
-
-impl BitOr for BitBoard
-{
-    type Output = BitBoard;
-
-    fn bitor(self, other: BitBoard) -> BitBoard
-    {
-        BitBoard( self.0 | other.0 )
-    }
-}
-
-impl BitAnd for BitBoard
-{
-    type Output = BitBoard;
-
-    fn bitand(self, other: BitBoard) -> BitBoard
-    {
-        BitBoard( self.0 & other.0 )
-    }
-}
-
-impl BitAndAssign for BitBoard
-{
-    fn bitand_assign(&mut self, other: Self)
-    {
-        *self = BitBoard( self.0 & other.0 )
-    }
-}
-
-impl BitOrAssign for BitBoard
-{
-    fn bitor_assign(&mut self, other: Self)
-    {
-        *self = BitBoard( self.0 | other.0 )
-    }
-}
-
-impl Not for BitBoard 
-{
-    type Output = BitBoard;
-    fn not(self) -> BitBoard 
-    {
-        BitBoard(!self.0)
-    }
-}
-
-impl fmt::Display for BitBoard
-{
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        for row in 0..8
-        {
-            write!(f, "\n")?;
-            for col in 0..8
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(f, "\n  abcdefgh")?;        
+            for row in 0..8
             {
+                write!(f, "\n {}",row)?;
+                for col in 0..8
+                {
 
-                if self.get_value_at(Coord::new(row, col))
-                {
-                    write!(f, "1")?;
+                    if self.black_board.get_value_at(Coord::new(row, col))
+                    {
+                        write!(f, "●")?;
+                    }
+                    else if self.white_board.get_value_at(Coord::new(row, col))
+                    {
+                        write!(f, "○")?;                    
+                    }
+                    else if self.moves.get_value_at(Coord::new(row, col))
+                    {
+                        write!(f, ",")?;                    
+                    }
+                    else 
+                    {
+                        write!(f, ".")?;                    
+                    }
                 }
-                else 
-                {
-                    write!(f, "0")?;
-                }
+                write!(f, "{}",row)?;            
             }
+            write!(f, "\n  abcdefgh")
         }
-        write!(f,"")
-    }
-}
-
-
-#[derive(Debug)]
-pub struct BoardIterator(BitBoard);
-
-impl Iterator for BoardIterator {
-    type Item = Coord;
-
-    fn next(&mut self) -> Option<Coord>
-    {
-        let pos = self.0.first_one()?;
-        self.0.set_value_at(pos, false);
-        Some(pos)
-    }
-}
-
-impl IntoIterator for BitBoard {
-    type Item = Coord;
-    type IntoIter = BoardIterator;
-
-    fn into_iter(self) -> Self::IntoIter 
-    {
-        BoardIterator(self)
-    }
-}
-
-//Tests
-//-----------------------------------------------------------------------------
-#[cfg(test)]
-mod test
-{
-    use super::*;
-
-    fn test_pattern( ) -> BitBoard
-    {
-        let mut board = BitBoard::empty();
-        board.set_value_at( Coord::new(0, 0), true );
-        board.set_value_at( Coord::new(0, 7), true );
-        board.set_value_at( Coord::new(7, 0), true );
-        board.set_value_at( Coord::new(7, 7), true );
-        board.set_value_at( Coord::new(2, 2), true );
-        board
     }
 
-    #[test]
-    fn test_board_creation()
-    {
-        //When bord is printed, rows are 'reversed' because msb is to the right
-        //So the coordinate system is reverse to what you would expect
-        assert_eq!(test_pattern(), BitBoard(0b10000001_00000000_00000000_00000000_00000000_00000100_00000000_10000001u64))
-    }
-
-    #[test]
-    fn test_print_board()
-    {
-        let out = format!("{}", test_pattern());
-        assert_eq!(out, "\n10000001\n00000000\n00100000\n00000000\n00000000\n00000000\n00000000\n10000001");
-    } 
-    #[test]
-    fn test_shift_right()
-    {
-        let out = format!("{}", test_pattern().shift_right());
-        assert_eq!(out, "\n01000000\n00000000\n00010000\n00000000\n00000000\n00000000\n00000000\n01000000");
-    } 
-    #[test]
-    fn test_shift_up()
-    {
-        let out = format!("{}", test_pattern().shift_up());
-        assert_eq!(out, "\n00000000\n00100000\n00000000\n00000000\n00000000\n00000000\n10000001\n00000000");
-    } 
-    #[test]
-    fn test_shift_left()
-    {
-        let out = format!("{}", test_pattern().shift_left());
-        assert_eq!(out, "\n00000010\n00000000\n01000000\n00000000\n00000000\n00000000\n00000000\n00000010");
-    } 
-    #[test]
-    fn test_shift_down()
-    {
-        let out = format!("{}", test_pattern().shift_down());
-        assert_eq!(out, "\n00000000\n10000001\n00000000\n00100000\n00000000\n00000000\n00000000\n00000000");
-    } 
 }
--- src/main.rs
+++ src/main.rs
@@ -1,102 +1,12 @@
 extern crate bit_board;
 use std::fmt;
-use bit_board::Coord;
-use bit_board::Direction;
-use bit_board::BitBoard;
+//use bit_board::bitboard::{Coord, Direction, BitBoard};
+use bit_board::Othello::OthelloSituation;
 
 
-enum Player
-{
-    Black,
-    White
-}
-
-struct OthelloSituation
-{
-    black_board: BitBoard,
-    white_board: BitBoard,
-    moves: BitBoard,
-    turn: Player
-}
-
-fn generate_moves_towards( own_board: BitBoard, other_board: BitBoard, direction: Direction ) -> BitBoard
-{
-    let empty_spaces = !own_board & !other_board;
-    let mut moves = BitBoard::empty();
-    let mut move_tmp = own_board.shift( direction ) & other_board;
-    while !(move_tmp.is_empty())
-    {
-        move_tmp = move_tmp.shift( direction );
-        let moves_generated_this_round = move_tmp & empty_spaces;
-        move_tmp &= other_board;
-        moves |= moves_generated_this_round;
-    }
-    moves
-}
-
-fn generate_moves( own_board: BitBoard, other_board: BitBoard ) -> BitBoard
-{
-    generate_moves_towards( own_board, other_board, Direction::Right ) |
-    generate_moves_towards( own_board, other_board, Direction::UpRight ) |
-    generate_moves_towards( own_board, other_board, Direction::Up ) |
-    generate_moves_towards( own_board, other_board, Direction::UpLeft ) |
-    generate_moves_towards( own_board, other_board, Direction::Left ) |
-    generate_moves_towards( own_board, other_board, Direction::DownLeft ) |
-    generate_moves_towards( own_board, other_board, Direction::Down ) |
-    generate_moves_towards( own_board, other_board, Direction::DownRight )
-
-}
-
-impl OthelloSituation
-{
-    fn new() -> OthelloSituation
-    {
-        let mut black_board = BitBoard::empty();
-        black_board.set_value_at(Coord::new(3,4),true);
-        black_board.set_value_at(Coord::new(4,3),true);
-        let mut white_board = BitBoard::empty();
-        white_board.set_value_at(Coord::new(3,3),true);
-        white_board.set_value_at(Coord::new(4,4),true);
-        let moves = generate_moves( black_board, white_board );
-        OthelloSituation{ black_board, white_board, moves, turn: Player::Black }
-    }
-}
-
-impl fmt::Display for OthelloSituation
-{
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "\n  abcdefgh")?;        
-        for row in 0..8
-        {
-            write!(f, "\n {}",row)?;
-            for col in 0..8
-            {
-
-                if self.black_board.get_value_at(Coord::new(row, col))
-                {
-                    write!(f, "●")?;
-                }
-                else if self.white_board.get_value_at(Coord::new(row, col))
-                {
-                    write!(f, "○")?;                    
-                }
-                else if self.moves.get_value_at(Coord::new(row, col))
-                {
-                    write!(f, ",")?;                    
-                }
-                else 
-                {
-                    write!(f, ".")?;                    
-                }
-            }
-            write!(f, "{}",row)?;            
-        }
-        write!(f, "\n  abcdefgh")
-    }
-}
 
 
-fn test_pattern( ) -> BitBoard
+/*fn test_pattern( ) -> BitBoard
 {
     let mut board = BitBoard::empty();
     board.set_value_at( Coord::new(0, 0), true );
@@ -105,10 +15,10 @@ fn test_pattern( ) -> BitBoard
     board.set_value_at( Coord::new(7, 7), true );
     board.set_value_at( Coord::new(2, 2), true );
     board
-}
+}*/
 
 fn main() {
-    let test_board = test_pattern();
+    /*let test_board = test_pattern();
     println!("Test pattern: {}", test_board);
     println!("Shift right: {}", test_board.shift_right());
     println!("Shift up: {}", test_board.shift_up());
@@ -119,7 +29,7 @@ fn main() {
     for coord in test_board
     {
         println!("{}", coord );
-    }
+    }*/
     println!("Board: {}", OthelloSituation::new());
     
 }
