217fc94c77e3a6d30c8b99452b7e73a9701c2a66
Antti Valli <Antti.Valli@gmail.com>
2018-04-06 23:00:37 +0300

    implement readinc from console for human player

--- src/lib.rs
+++ src/lib.rs
@@ -26,8 +26,42 @@ pub mod game
     }
 }
 
+
+use std::io;
 use othello::OthelloSituation;
 use game::GameSituation;
+
+fn read_coord() -> Option<bitboard::Coord>
+{
+    let mut input_coord: Option<bitboard::Coord> = None;
+	while input_coord.is_none()
+	{
+	    println!("Please enter coord (eg. a1) or 'pass'");       
+		let mut input = String::new();
+	    io::stdin()
+	        .read_line(&mut input)
+	        .expect("failed to read input.");
+
+        if input.trim() == "pass"
+        {
+            return None;
+        }
+
+		let chars:Vec<char> = input.trim().chars().collect();
+
+		let col = "abcdefgh".find(chars[0]);
+		let row = chars[1].to_digit(10).unwrap_or(0) - 1;
+
+        if let Some(col) = col 
+        {
+            if row >= 0 && row < 8
+            {
+                input_coord = Some(bitboard::Coord::new( row, col as u32 ));
+            } 
+        }
+	}
+	input_coord
+}
 
 struct HumanOthelloPlayer{}
 
@@ -38,6 +72,12 @@ impl game::Player for HumanOthelloPlayer
     fn make_move( &mut self, situation: &Self::Situation, previous_move: Option<Self::Move> ) -> Option<Self::Move>
     {
         println!("{}", situation);
-        situation.get_moves().next()
+        let mut my_move = read_coord()?;
+        while situation.copy_apply(my_move).is_none() 
+        {
+            println!("Invalid move");
+            my_move = read_coord()?;
+        }
+        Some(my_move)
     }
 }
